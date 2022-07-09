e98f0e7b860a658742bcb6115972f476f136868c
disha <mumudb@gmail.com>
2022-07-09 15:48:04 -0400

    react-app created

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
-  extends: 'standard',
+  "extends": [
+    "react-app",
+    "react-app/jest",
+    "eslint:recommended", 
+    "plugin:react/recommended"],
   rules: {
-    semi: [2, 'always']
+    semi: [2, 'always'],
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
   }
 };
