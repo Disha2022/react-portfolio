93e6a7e0a19d43483672183912141486bb7c4c20
disha <mumudb@gmail.com>
2022-07-16 16:00:59 -0400

    add Project

---
+++ src/components/Project/index.js
@@ -0,0 +1,17 @@
+
+const Project = ({ project }) => {
+  const { name, description, imgName} = project;
+
+  return (
+      <div className="container">
+        <h3 className="title">{name}</h3>
+        <img
+          src={require(`../../assets/large/${imgName}.jpg`).default}
+          alt="current category"
+        />
+        <p>{description}</p>
+      </div>
+  );
+};
+
+export default Project;
