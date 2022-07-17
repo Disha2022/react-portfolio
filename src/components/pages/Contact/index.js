696d4917f0f941fd9148de1670f6edab0962a6bd
disha <mumudb@gmail.com>
2022-07-16 20:37:40 -0400

    add contact and resume content

--- src/components/pages/Contact/index.js
+++ src/components/pages/Contact/index.js
@@ -5,6 +5,21 @@ function Contact() {
             <h2>
                 Contact
             </h2>
+            <form>
+                <label>
+                    Name:
+                    <input type="text" name="name" />
+                </label>
+                <label>
+                    Email address:
+                    <input type="email" name="email" />
+                </label>
+                <label>
+                    Message:
+                    <textarea id="message" name="message" rows="10" />
+                </label>
+                <input type="submit" value="Submit" disabled />
+            </form>
         </>
     );
 }
--- src/components/pages/Resume/index.js
+++ src/components/pages/Resume/index.js
@@ -7,8 +7,32 @@ function Resume() {
             <p>
                 Download my <a href="/assets/resume.pdf" download>resume</a>
             </p>
+            <h3>
+                Front-end Proficiencies
+            </h3>
+            <ul>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>JQuery</li>
+                <li>Bootstrap</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>responsive design</li>
+            </ul>
+
+            <h3>
+                Back-end Proficiencies
+            </h3>
+            <ul>
+                <li>NodeJS</li>
+                <li>ExpressJS</li>
+                <li>APIs</li>
+                <li>MongoDB</li>
+                <li>MySQL</li>
+            </ul>
         </>
     );
 }
+
 
 export default Resume;
