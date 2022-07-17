ff02963f6c4b6735ac8e332a563f074bca123cb3
disha <mumudb@gmail.com>
2022-07-17 16:22:32 -0400

    add hover to project

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,22 +1,34 @@
+import { useState } from "react";
 import "./Project.css";
 
 // used multiple times in the Portfolio section
 const Project = ({ project }) => {
   const { name, description, imgName, gitLink, deployLink } = project;
-
+  const [isShown, setShow] = useState(false);
+  function show() {
+    setShow(true);
+  }
+  function hide() {
+    setShow(false);
+  }
   return (
     <div className="projectContainer">
 
       <img
         src={require(`../../assets/projects/${imgName}`).default}
         alt={name}
+        onMouseEnter={show}
+        onMouseLeave={hide}
       />
-      <div className="textContainer">
-        <h3><a href={gitLink}>{name}
-          <img alt="github logo" src={require(`../../assets/github.png`).default}></img>
-        </a></h3>
-        <p><a href={deployLink}>{description}</a></p>
-      </div>
+      {isShown && (
+        <div className="textContainer" onMouseEnter={show} onMouseLeave={hide}>
+          <h3><a href={deployLink}>{name}</a><a href={gitLink}>
+            <img className="gitImg" alt="github logo" src={require(`../../assets/github.png`).default}></img>
+          </a></h3>
+          <p>{description}</p>
+        </div>
+      )}
+
     </div>
   );
 };
