b43831673ebd1b8eccbc5180e54df9451a1e6312
disha <mumudb@gmail.com>
2022-07-16 20:41:18 -0400

    add placeholder aboutme content

--- src/components/pages/About/index.js
+++ src/components/pages/About/index.js
@@ -1,10 +1,17 @@
-
+import logo from "../../../assets/logo.svg";
 function About() {
     return (
         <>
             <h2>
                 About me.
             </h2>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                Awesome description 1.
+            </p>
+            <p>
+                Awesome description 2.
+            </p>
         </>
     );
 }
